41548d6118cb0198a8f2309b67a0c2724e86aad0
JordanYoungCodes <168236369+JordanYoungCodes@users.noreply.github.com>
2024-06-08 23:30:30 -0400

    new changes

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -10,7 +10,31 @@ dTheme.addEventListener("click", function(event) {
     }else{
         document.body.classList ="dark"
         event.target.setAttribute("data-theme","dark")
-
+            return;
     }
 
 })
+
+
+// trying to get input from form
+//const submitBtn = document.querySelector("#submitBtn");
+let title1 = document.getElementById(title1).textContent;
+let userName1 = document.querySelector("#UserName1").textContent;
+let blog1 = document.querySelector('#Notes').value;
+
+
+localStorage.setItem("blog", blog1);
+blog1.textContent = blog1;
+localStorage.setItem("title", title1);
+
+localStorage.setItem("UserName", userName1);
+    userName1.textContent = userName1;
+//  //submitBtn.addEventListener('click',function(event) {
+//     event.preventDefault(); 
+    
+    
+console.log("title1");
+    
+    
+    
+//     
